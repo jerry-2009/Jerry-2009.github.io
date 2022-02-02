e6941786dafd486164e11cfa576e9df6a7f1021e
jerry-2009 <77391147+jerry-2009@users.noreply.github.com>
2022-02-02 14:48:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var $ = mdui.$;
+var inst = new mdui.Drawer('#drawer');
+
+// method
+
+
+$('#toggle').on('click', function () {
+  inst.toggle();
+});
+$('#login').on('open.mdui.dialog', function () {
+  tab.handleUpdate();
+});
+// event
+$('#drawer').on('open.mdui.drawer', function () {
+  console.log('open');
+});
+
+$('#drawer').on('opened.mdui.drawer', function () {
+  console.log('opened');
+});
+
+$('#drawer').on('close.mdui.drawer', function () {
+  console.log('close');
+});
+
+$('#drawer').on('closed.mdui.drawer', function () {
+  console.log('closed');
+});
